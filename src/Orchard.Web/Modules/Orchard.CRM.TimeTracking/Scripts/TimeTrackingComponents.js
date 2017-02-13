b012f1c64926436d25c3ab37c37f97652d4b91cc
Siyamand Ayubi <sayubi@globalblue.com>
2017-02-13 16:33:29 +0100

    start adding JSX files

---
+++ src/Orchard.Web/Modules/Orchard.CRM.TimeTracking/Scripts/TimeTrackingComponents.js
@@ -0,0 +1,12 @@
+
+window.orchardcollaboration = window.orchardcollaboration || {};
+var orchardcollaboration = window.orchardcollaboration;
+orchardcollaboration.react = orchardcollaboration.react || {};
+orchardcollaboration.react.allComponents = orchardcollaboration.react.allComponents || {};
+
+(function () {
+
+    var TimeTrackingList = React.createClass({
+        displayName: "TimeTrackingList"
+    });
+})();
